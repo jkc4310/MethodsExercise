24bb9fbe8ee353d5d860493861f3ac6f87e417d3
kendall chapman <jkc4310@gmail.com>
2023-07-05 15:39:27 -0400

    Exercise two updates

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,12 +2,43 @@
 
 namespace MethodsExercise
 {
-    public class Program
-       
+     
+    class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int Minus(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Mod(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
         static void Main(string[] args)
         {
-            
+            var amountOfCars = Sum(2, 6);
+            var something = Multiply(60, 2, 4);
+            var divide = Divide(144, 12);
+            var Subtract = Minus(9, 3);
+            var Modulus = Mod(5, 2);
+
             // --------------Exercise 1--------------
             //Name: Kendall
             //Your Favorite Color
@@ -33,6 +64,8 @@ namespace MethodsExercise
             Console.WriteLine($"Favorite Color: {color}");
             Console.WriteLine($"Favorite Animal: {animal}");
             Console.WriteLine($"favorite Band: {band}");
+
+            // --------------Exercise 2--------------
 
 
         }
